113d51636e1cf2dd9e945eea1120ee9ac0ec74b3
Matheusdnf <matheusdiniz870@gmail.com>
2025-10-01 20:49:56 -0300

    feat: validação do mes

--- valida.py
+++ valida.py
@@ -1,4 +1,5 @@
 import re
+from telas import menu_meses
 
 
 def verifica_ano(ano):
@@ -7,7 +8,6 @@ def verifica_ano(ano):
     if not match:
         return False
     return ano
-
 
 def verifica_turno(turno):
     turnos_dict = {
@@ -19,3 +19,18 @@ def verifica_turno(turno):
         return False
     else:    
         return turnos_dict[turno]
+    
+def verifica_mes(mes):
+    meses_dict = {
+        "1": "1-JANEIRO", "2": "2-FEVEREIRO", "3": "3-MARÇO", "4": "4-ABRIL",
+        "5": "5-MAIO", "6": "6-JUNHO", "7": "7-JULHO", "8": "8-AGOSTO",
+        "9": "9-SETEMBRO", "10": "10-OUTUBRO", "11": "11-NOVEMBRO", "12": "12-DEZEMBRO"
+    }
+    padrao_mes = r"^(1[0-2]|[1-9])"
+    match = re.match(padrao_mes, mes)
+    if not match:
+        return False
+    else:
+        mes_num = match.group()
+        return meses_dict[mes_num]
+
