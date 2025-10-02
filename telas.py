e21774da857c84bea2896344c90e7746b6747bc4
Matheusdnf <matheusdiniz870@gmail.com>
2025-10-01 21:34:16 -0300

    feat: telas

---
+++ telas.py
@@ -0,0 +1,23 @@
+
+def tela_apresentacao():
+    print("""
+    ██████  ██████  ███    ███ ██████   █████   ██████ ████████  █████  ██████   ██████  ██████      ██████  ███████     ██████  ██████  ███████ 
+    ██      ██    ██ ████  ████ ██   ██ ██   ██ ██         ██    ██   ██ ██   ██ ██    ██ ██   ██     ██   ██ ██          ██   ██ ██   ██ ██      
+    ██      ██    ██ ██ ████ ██ ██████  ███████ ██         ██    ███████ ██   ██ ██    ██ ██████      ██   ██ █████       ██████  ██   ██ █████   
+    ██      ██    ██ ██  ██  ██ ██      ██   ██ ██         ██    ██   ██ ██   ██ ██    ██ ██   ██     ██   ██ ██          ██      ██   ██ ██      
+    ██████   ██████  ██      ██ ██      ██   ██  ██████    ██    ██   ██ ██████   ██████  ██   ██     ██████  ███████     ██      ██████  ██      
+    """)
+    print("Feito por Matheus Diniz")
+
+
+def meu_menu():
+    print("""
+    Selecione uma opção:
+    1. Criar pastas para um novo ano
+    2. Relatório do mês
+    3. Sair
+        
+    """)
+
+def menu_meses():
+    print("1 - Janeiro\n2 - Fevereiro\n3 - Março\n4 - Abril\n5 - Maio\n6 - Junho\n7 - Julho\n8 - Agosto\n9 - Setembro\n10 - Outubro\n11 - Novembro\n12 - Dezembro")
